348f46c9e85666a227042e86afcb88deed4ef6e7
James Lockhart <james.lockhart@shopify.com>
2024-06-13 16:00:42 -0400

    add swizzle

--- Sources/PackageManager.swift
+++ Sources/PackageManager.swift
@@ -1,5 +1,12 @@
 public struct PackageManager {
-    public init() {
-      print("PackageManagerTest is here")
+    public init() {}
+}
+
+extension URLSession {
+    @objc func swizzled_dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        print("What a nice request you have there!")
+        
+        // Call the original method
+        return swizzled_dataTask(with: request, completionHandler: completionHandler)
     }
 }
